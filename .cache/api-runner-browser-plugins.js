6f3c0cf8eb60131242bc003b58043eee1fda55e7
Ma7eer <maher.alkendi2@gmail.com>
2018-11-03 14:14:00 -0600

    deleted the node_modules file and added .gitignore file

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('E:/Maher-Projects/hackathon-1/barter_hub-freecodecamp-jamstackhackathon2018/node_modules/gatsby-plugin-offline/gatsby-browser'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('E:/Maher-Projects/hackathon-1/barter_hub-freecodecamp-jamstackhackathon2018/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/async-requires.js
@@ -0,0 +1,15 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("E:\\Maher-Projects\\hackathon-1\\barter_hub-freecodecamp-jamstackhackathon2018\\.cache\\dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("E:\\Maher-Projects\\hackathon-1\\barter_hub-freecodecamp-jamstackhackathon2018\\src\\pages\\404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-app-js": () => import("E:\\Maher-Projects\\hackathon-1\\barter_hub-freecodecamp-jamstackhackathon2018\\src\\pages\\app.js" /* webpackChunkName: "component---src-pages-app-js" */),
+  "component---src-pages-index-js": () => import("E:\\Maher-Projects\\hackathon-1\\barter_hub-freecodecamp-jamstackhackathon2018\\src\\pages\\index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-login-js": () => import("E:\\Maher-Projects\\hackathon-1\\barter_hub-freecodecamp-jamstackhackathon2018\\src\\pages\\login.js" /* webpackChunkName: "component---src-pages-login-js" */),
+  "component---src-pages-page-2-js": () => import("E:\\Maher-Projects\\hackathon-1\\barter_hub-freecodecamp-jamstackhackathon2018\\src\\pages\\page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */),
+  "component---src-pages-profile-js": () => import("E:\\Maher-Projects\\hackathon-1\\barter_hub-freecodecamp-jamstackhackathon2018\\src\\pages\\profile.js" /* webpackChunkName: "component---src-pages-profile-js" */)
+}
+
+exports.data = () => import("E:\\Maher-Projects\\hackathon-1\\barter_hub-freecodecamp-jamstackhackathon2018\\.cache\\data.json")
+
