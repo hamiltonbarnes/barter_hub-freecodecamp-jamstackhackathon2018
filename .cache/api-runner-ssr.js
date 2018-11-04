734d267e9da066956ac67050bf52357c25fee206
Gopi Krishna <Gopiwayz@gmail.com>
2018-11-04 10:39:01 +0530

    Footer Fixed

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,8 +1,8 @@
 var plugins = [{
-      plugin: require('F:/hackathon/barter_hub-freecodecamp-jamstackhackathon2018/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      plugin: require('/mnt/f/hackathon/barter_hub-freecodecamp-jamstackhackathon2018/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('F:/hackathon/barter_hub-freecodecamp-jamstackhackathon2018/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
+      plugin: require('/mnt/f/hackathon/barter_hub-freecodecamp-jamstackhackathon2018/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/gatsby-icon.png"},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
