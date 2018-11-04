28804d966aef8a256482abb22585eef3ebce2020
Gopi Krishna <Gopiwayz@gmail.com>
2018-11-04 18:43:42 +0530

    Products Page Completed (Images and Links are pending)

--- src/components/product-item.js
+++ src/components/product-item.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import Link from 'gatsby'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 
-
-const ProductItem = ({ item,price,link }) => {
-  price = price ? " - "+ price: price
+const ProductItem = ({ item, price, link }) => {
+  price = price ? ' - ' + price : price
   return (
     <div className="product-item">
       <div className="product-image">
         <img src={item.img} alt={item.value} />
       </div>
       <div className="product-description">
-        <a href={link}>{item.value}  {price}</a>
+        <a href={link}>
+          {item.value} {price}
+        </a>
       </div>
     </div>
   )
@@ -19,13 +20,11 @@ const ProductItem = ({ item,price,link }) => {
 
 ProductItem.propTypes = {
   price: PropTypes.string,
-
-  
-};
+  link: PropTypes.string,
+}
 
 ProductItem.defaultProps = {
   price: '',
-  link: "/productpage"
-  
-};
+  link: '/productpage',
+}
 export default ProductItem
