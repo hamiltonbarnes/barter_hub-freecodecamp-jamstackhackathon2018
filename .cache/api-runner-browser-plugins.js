6d6ea0dd33dfb641ded4b93a040c889925624b4e
hamiltonbarnes <hamiltonbarnes4@gmail.com>
2018-11-04 07:39:03 +0000

    removing node_moudles and cache files

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/mnt/f/hackathon/barter_hub-freecodecamp-jamstackhackathon2018/node_modules/gatsby-plugin-offline/gatsby-browser'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/mnt/f/hackathon/barter_hub-freecodecamp-jamstackhackathon2018/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
